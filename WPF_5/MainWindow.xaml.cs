83aedb9ab4c16b1fbfa2bc8a36d9a3b27888bc20
4b2g0039@office.stust.edu.tw <4b2g0039@office.stust.edu.tw>
2024-12-10 17:11:46 +0800

    新增學生資料綁定功能及 UI 元件
    在 `MainWindow.xaml` 中，新增了 `Grid.RowDefinitions` 和 `Grid.ColumnDefinitions`，並在 `Grid` 中新增了一個 `StackPanel` 和一個 `StatusBar`。`StackPanel` 包含一個 `Label` 和一個 `ComboBox`，`StatusBar` 包含一個 `StatusBarItem` 和一個 `Label`。
    
    在 `MainWindow.xaml.cs` 中，移除了不必要的 `using` 指令，並新增了一個 `List<Student>` 來儲存學生資料。在 `MainWindow` 的建構子中，呼叫了 `InitializeData` 方法來初始化學生資料，並將資料綁定到 `ComboBox`。
    
    新增了一個 `Student.cs` 檔案，定義了一個 `Student` 類別，包含 `StudentId` 和 `StudentName` 屬性，並覆寫了 `ToString` 方法來回傳學號和學生姓名。

--- WPF_5/MainWindow.xaml.cs
+++ WPF_5/MainWindow.xaml.cs
@@ -1,13 +1,4 @@
-﻿using System.Text;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows;
 
 namespace WPF_5
 {
@@ -16,9 +7,20 @@ namespace WPF_5
     /// </summary>
     public partial class MainWindow : Window
     {
+        List<Student> students = new List<Student>();
         public MainWindow()
         {
             InitializeComponent();
+            InitializeData();
+        }
+
+        private void InitializeData()
+        {
+           students.Add(new Student { StudentId = "S001", StudentName = "陳小明" });
+           students.Add(new Student { StudentId = "S002", StudentName = "林小華" });
+           students.Add(new Student { StudentId = "S003", StudentName = "張小英" });
+            cmbStudent.ItemsSource = students;
+            cmbStudent.SelectedIndex = 0;//預設選取第一筆資料
         }
     }
 }
---
+++ WPF_5/Student.cs
@@ -0,0 +1,12 @@
+﻿namespace WPF_5
+{
+    class Student
+    {   //定義所需要的資料
+        public string StudentId { get; set; }//public一個存取的方法 字串 學號 get set可存可取
+        public string StudentName { get; set; }//public一個存取的方法 字串 學生姓名 get set可存可取
+        public override string ToString()//覆寫ToString方法
+        {
+            return $"{StudentId} {StudentName}";//回傳學號+學生姓名
+        }
+    }
+}
